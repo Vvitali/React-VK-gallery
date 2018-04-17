3778dc537c5a92e8f5e173382610252a7cfff6ca
Vvitali <vvitali26@gmail.com>
2018-04-16 21:57:48 -0400

    first request to VK

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,16 @@
 (function(){
+
+	function httpGet(theUrl){
+		var xmlHttp = new XMLHttpRequest();
+		xmlHttp.open("GET", theUrl, false);
+		xmlHttp.send(null);
+		return xmlHttp.responseText;
+	}
 	VK.init({
 		apiId: "6450788"
 	});
+
+	var data = httpGet("https://api.vk.com/method/users.get?user_ids=210700286&fields=bdate&v=5.74");
+	console.log(data)
+
 })()
